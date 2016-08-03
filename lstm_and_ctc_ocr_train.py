605a010764313ff13ab5abb6b22308fd1c27864f
AndyWang <synckey@gmail.com>
2016-08-03 18:53:34 +0800

    5 digits okk
    Former-commit-id: 1aab8e270de6f8ac1ca2d5762175fa7b165b7337

--- lstm_and_ctc_ocr_train.py
+++ lstm_and_ctc_ocr_train.py
@@ -104,10 +104,10 @@ def train():
             train_cost = train_ler = 0
             for batch in xrange(common.BATCHES):
                 start = time.time()
-                train_inputs, train_targets, train_seq_len = utils.get_data_set('train', batch,
+                train_inputs, train_targets, train_seq_len = utils.get_data_set('train', batch * common.BATCH_SIZE,
                                                                                 (batch + 1) * common.BATCH_SIZE)
 
-                print("get data time",time.time() - start )
+                print("get data time", time.time() - start)
                 start = time.time()
                 c, steps = do_batch()
                 train_cost += c * common.BATCH_SIZE
