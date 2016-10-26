a31b9ad6bb049f83a2d327e7539772d5937ff030
yuan-wenhua <32109175@qq.com>
2016-10-26 14:15:44 +0800

    change reversal code to binaryzation code
    Former-commit-id: 4243037ef0faa519d36cc808593d0ebed79ef4c9
    Former-commit-id: 60269fa204d7f9c61409a689fde4c474a62ee5f6 [formerly 9fb045a5e36dc49e343b5a120e3a5e9dcbc86757]
    Former-commit-id: edf269d6e700268fd510eae1a6c1475cf709965b

--- bgs/processTestImg.py
+++ bgs/processTestImg.py
@@ -9,5 +9,9 @@ if __name__ == '__main__':
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         for idx, line in enumerate(gray):
             for idy, item in enumerate(line):
-                gray[idx][idy] = 255 - gray[idx][idy]
+                if gray[idx][idy] >= 150:
+                    gray[idx][idy] = 255
+                else:
+                    gray[idx][idy] = 0
+                
         cv2.imwrite(fname, gray)
